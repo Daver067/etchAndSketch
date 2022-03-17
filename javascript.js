3f59eecfc9f891290bfa63db185120436aa3b67f
Dave <davediemert_88@hotmail.com>
2022-03-16 18:51:44 -0700

    Erase and Create grid are working however I am having an issue with the Second erase in a row causing an error (only does it once). The error stems from the container node having a ghost child node. Commiting to see if someone can help me out with it.

--- javascript.js
+++ javascript.js
@@ -2,20 +2,30 @@ const reset = document.querySelector('#reset');
 const newSize = document.querySelector('#newSize');
 const randomColor = document.querySelector('#randomColor');
 const container = document.getElementById('container')
+let gridSize = 16;
 
 reset.addEventListener('click', () => {
-    alert('reset clicked');
+    eraseGrid();
 });
+
 newSize.addEventListener('click', () =>{
-    alert('newSize Clickereded');
+    eraseGrid();
+    gridSize = prompt('enter a number from 1-100 for grid size');
+    gridSize < 101 ? createGrid(gridSize): alert('enter number between 1-100 please.');
 });
 randomColor.addEventListener('click', () => {
     alert('randomColor clicked');
 });
 
+function eraseGrid(){
+    if (container.firstChild == null) {
+        return;
+    }
+    else {for (i = 0; i < gridSize; i++)
+    container.removeChild(container.lastChild);
+}}
 
-function createGrid(){
-    let gridSize = 5;
+function createGrid(gridSize){
     for (let i = 1; i <= gridSize; i++){
         let newRow = document.createElement('div');
         newRow.classList.add('row');
@@ -26,4 +36,5 @@ function createGrid(){
         newRow.appendChild(newCell);
     }}
 }
-createGrid()
+console.log(container.hasChildNodes())
+console.log(container.firstChild)
